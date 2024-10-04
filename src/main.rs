351633b716a46b6aa75c94e01d14571b8d430cbb
Neh Jain <nehjain.2001@gmail.com>
2024-10-04 23:24:04 +0530

    CHANGES

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = || App::new().service(hello);
+
+    HttpServer::new(app).bind(("127.0.0.1", 5000))?.run().await
 }
